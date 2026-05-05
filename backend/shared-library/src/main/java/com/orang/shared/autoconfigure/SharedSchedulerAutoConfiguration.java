a2633ee2c9361fb3f8177e636097d26ab5fba109
Noka993 <Noka993@wp.pl>
2026-05-05 12:55:20 +0200

    Fix Scheduler Lock configuration

--- backend/shared-library/src/main/java/com/orang/shared/autoconfigure/SharedSchedulerAutoConfiguration.java
+++ backend/shared-library/src/main/java/com/orang/shared/autoconfigure/SharedSchedulerAutoConfiguration.java
@@ -4,20 +4,29 @@ import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.redis.spring.RedisLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 
+import java.util.Optional;
+
 @AutoConfiguration
-@ConditionalOnClass({LockProvider.class, RedisConnectionFactory.class})
-@ConditionalOnMissingBean(RedisConnectionFactory.class)
+@ConditionalOnClass(LockProvider.class)
 @EnableSchedulerLock(defaultLockAtMostFor = "10m")
 public class SharedSchedulerAutoConfiguration {
 
     @Bean
+    @ConditionalOnBean(RedisConnectionFactory.class)
     @ConditionalOnMissingBean
     public LockProvider lockProvider(RedisConnectionFactory connectionFactory) {
         return new RedisLockProvider(connectionFactory, "orangchat-lock");
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(LockProvider.class)
+    public LockProvider fallbackLockProvider() {
+        return (lockConfiguration) -> Optional.empty();
     }
 }
